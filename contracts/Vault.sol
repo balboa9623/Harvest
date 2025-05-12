039c40644548ed9f50f4d372a38285f048a41895
balboa9623 <mathewrichu16@gmail.com>
2025-05-12 01:34:34 -0500

    Withdrawal working version. Fixing issue with deposit

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,31 +2,48 @@
 pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol"; // To be deleted. for testing purpose.
+
 contract Vault {
-    mapping (address => uint256) private balances;
-    uint256 public minUSD = 5 * 1e18;
+    // Track balances per user per token
+    mapping(address => uint256)  balances;
+    uint256 public minUSD = 1 * 1e18;
+    address constant PRICE_FEED_SEPOLIA = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    // IERC20 public immutable token = IERC20(_sepoliaAddress);  // ERC-20 token this vault accepts
+    
 
     // 'emit' event listener
     event Deposit(address indexed user, uint256 amount);
-    event Withdrawal(address indexed user, uint256 amount);
+    event Deposit(address indexed user, address indexed tokenAddress, uint256 amount);
 
+    event Withdrawal(address indexed user, uint256 amount);
+    event Withdrawal(address indexed user, address indexed tokenAddress, uint256 amount);
+
+    event UserBalanceCheckByAccount(address enquirerAddress, address accountHolderAddress);
+    
+    
     function deposit() public payable {
-        require(msg.value > 0, "Amount depositing should be greater than 1");
-        balances[msg.sender] += msg.value;            // update balance
-        emit Deposit(msg.sender, msg.value);          // log deposit event
+        require(msg.value > 0, "No ETH sent");
+        uint256 usdValue = getConvertionRate(msg.value);
+        require(usdValue >= minUSD, "Deposit must be at least $1 worth of ETH.");
+
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw() public {
+
+    function withdraw() public payable {
         uint256 userBalance = balances[msg.sender];
         require(userBalance > 0, "Nothing to withdraw");
-
+        
         // Set balance to 0 first (protects against re-entrancy)
         balances[msg.sender] = 0;
 
         // Now try to send ETH
         (bool success, ) = payable(msg.sender).call{value: userBalance}("");
         require(success, "Withdrawal failed");
-
+        
         emit Withdrawal(msg.sender, userBalance);              // log withdrawal event
     }
 
@@ -34,22 +51,28 @@ contract Vault {
         return balances[msg.sender];
     }
 
-    function getPriceETH_USD() private view returns (uint256) {
-        /**
-        * Network: Sepolia
-        * Aggregator: ETH/USD
-        * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        */
-        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        
-        (, int256 answer, , ,) = dataFeed.latestRoundData(); // Price of ETH in USD
-        // msg.value and answer has different decimal places. answer has 8 decimal places (you can also call decimal() function. msg.value has 18.
-        return uint256(answer) * 1e10;
+    function checkBalanceWithAddress(address userAddress) private returns (uint256) {
+        emit UserBalanceCheckByAccount(msg.sender, userAddress);
+        return balances[userAddress];
     }
 
-    function getConvertionRate(uint256 ethAmount) private view returns (uint256) {
+    // 2000000000000000 = $5.03
+    function getPriceETH_USD() public view returns (uint256) {
+        
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            PRICE_FEED_SEPOLIA
+        );
+        
+        ( , int256 answer, , , ) = priceFeed.latestRoundData();
+        
+        require(answer > 0, "Invalid price feed");
+        uint256 scaled = uint256(answer) * 1e10;
+        return scaled;
+    }
+
+    function getConvertionRate(uint256 ethAmount) public view returns (uint256) {
         uint256 ethPrice = getPriceETH_USD();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
+        uint256 usd = (ethPrice * ethAmount) / 1e18;
+        return usd;
     }
 }
